592ce5bc14f03375c55d28aa64a4ca817677bea4
Loïc Delmaire <loic.delmaire@gmail.com>
2023-01-23 20:20:20 +0100

    Introduce bin/import_latest_open_api_files.sh
    Import all OpenAPI files too

---
+++ bin/import_latest_open_api_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: $0 /path/to/siade"
+  exit 1
+fi
+
+path=$1
+
+cp $path/public/v2/open-api.yml openapi_files/api_entreprise_v2.yaml
+cp $path/swagger/openapi.yaml openapi_files/api_entreprise.yaml
+cp $path/swagger/api-particulier-open-api.yml openapi_files/api_particulier.yaml
