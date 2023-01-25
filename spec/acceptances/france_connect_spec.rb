3f3cf0f4c5c66f4730920fdc1e52e678467f30b2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-01-25 14:13:05 +0100

    Enhance tests for FranceConnect

--- spec/acceptances/france_connect_spec.rb
+++ spec/acceptances/france_connect_spec.rb
@@ -1,34 +1,36 @@
 RSpec.describe 'FranceConnect specs' do
   Dir[File.join(root_path, 'payloads/france_connect/*.yaml')].each do |payload|
-    it 'is a valid YAML file' do
-      expect { YAML.load_file(payload) }.not_to raise_error
-    end
+    describe "Payload #{File.basename(payload)}" do
+      it 'is a valid YAML file' do
+        expect { YAML.load_file(payload) }.not_to raise_error
+      end
 
-    it 'has a valid json for "payload" key' do
-      data = YAML.load_file(payload)
+      it 'has a valid json for "payload" key' do
+        data = YAML.load_file(payload)
 
-      expect { JSON.parse(data['payload']) }.not_to raise_error
-    end
+        expect { JSON.parse(data['payload']) }.not_to raise_error
+      end
 
-    it 'has a 200 status code' do
-      data = YAML.load_file(payload)
+      it 'has a 200 status code' do
+        data = YAML.load_file(payload)
 
-      data['status'] == 200
-    end
+        data['status'] == 200
+      end
 
-    it 'has only a token param' do
-      data = YAML.load_file(payload)
+      it 'has only a token param' do
+        data = YAML.load_file(payload)
 
-      data['params'].keys == ['token']
-      expect(data['params']['token']).to be_a(String)
-    end
+        data['params'].keys == ['token']
+        expect(data['params']['token']).to be_a(String)
+      end
 
-    it 'has a valid payload according to OpenAPI file' do
-      data = YAML.load_file(payload)
-      schema = File.read(File.join(root_path, 'spec', 'france_connect_json_schema.json'))
-      test = JSON::Validator.fully_validate(schema, JSON.parse(data['payload']))
+      it 'has a valid payload according to OpenAPI file' do
+        data = YAML.load_file(payload)
+        schema = File.read(File.join(root_path, 'spec', 'france_connect_json_schema.json'))
+        test = JSON::Validator.fully_validate(schema, JSON.parse(data['payload']))
 
-      expect(test).to be_empty, "JSON schema validation failed: #{test}"
+        expect(test).to be_empty, "JSON schema validation failed: #{test}"
+      end
     end
   end
 end
