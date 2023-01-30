b0e8fe6f7bc0b771491e4948268dd8437b4f6e52
Loïc Delmaire <loic.delmaire@gmail.com>
2023-01-30 20:34:16 +0100

    Can generate READMEs for each payload folder
    For each folder, there's a title, optional description, and for each
    payload file:
    
    * Name
    * Params (collapsed)
    * Payload (collapsed)
    * cURL command (collapsed)

--- bin/generate_payload_readme.rb
+++ bin/generate_payload_readme.rb
@@ -4,36 +4,42 @@ lib_folder = File.expand_path('../lib', File.dirname(__FILE__))
 $LOAD_PATH.unshift(lib_folder) unless $LOAD_PATH.include?(lib_folder)
 
 require 'open_api_helpers'
+require 'generate_payload_readme'
 
 include OpenAPIHelpers
 
-readme_path = File.expand_path('../payloads/README.md', File.dirname(__FILE__))
-readme_stream = File.open(readme_path, 'w')
+main_readme_path = File.expand_path('../payloads/README.md', File.dirname(__FILE__))
+main_readme_stream = File.open(main_readme_path, 'w')
 
-readme_stream.puts '# Payloads'
-readme_stream.puts
-readme_stream.puts 'Les chemins sans lien ne possèdent pas de payload: l\'environnement de test'
-readme_stream.puts 'génère une payload par défaut basée sur la spécification OpenAPI associée.'
+main_readme_stream.puts '# Payloads'
+main_readme_stream.puts
+main_readme_stream.puts 'Les chemins sans lien ne possèdent pas de payload: l\'environnement de test'
+main_readme_stream.puts 'génère une payload par défaut basée sur la spécification OpenAPI associée.'
 
 {
   'api_particulier' => 'API Particulier',
   'api_entreprise' => 'API Entreprise v3+',
 }.each do |api_id, api_name|
-  readme_stream.puts
-  readme_stream.puts("## #{api_name}")
-  readme_stream.puts
+  main_readme_stream.puts
+  main_readme_stream.puts("## #{api_name}")
+  main_readme_stream.puts
 
   schema = load_schema(api_id)
 
-  schema['paths'].each do |path, path_spec|
-    operation_id = path_spec['get']['responses']['200']['x-operationId']
-    title = path_spec['get']['summary']
+  schema['paths'].each do |api_path, api_path_spec|
+    operation_id = api_path_spec['get']['responses']['200']['x-operationId']
     next unless operation_id
 
+    title = api_path_spec['get']['summary']
+
     if File.exist?(File.join(root_path, 'payloads', operation_id))
-      readme_stream.puts("* [#{title}](#{operation_id}) (`#{path}`)")
+      main_readme_stream.puts("* [#{title}](#{operation_id}) (`#{api_path}`)")
+
+      GeneratePayloadReadme.new(operation_id, api_path_spec['get']['summary'], api_path).perform
     else
-      readme_stream.puts("* #{title} (`#{path}`)")
+      main_readme_stream.puts("* #{title} (`#{api_path}`)")
     end
   end
 end
+
+GeneratePayloadReadme.new('france_connect', 'France Connect').perform
---
+++ lib/generate_payload_readme.rb
@@ -0,0 +1,123 @@
+require 'open_api_helpers'
+require 'generate_code_sample_from_path'
+
+class GeneratePayloadReadme
+  include OpenAPIHelpers
+
+  attr_reader :operation_id, :title, :api_path
+
+  def initialize(operation_id, title, api_path = nil)
+    @operation_id = operation_id
+    @title = title
+    @api_path = api_path
+  end
+
+  def perform
+    add_title
+    add_description
+
+    payload_entries.each do |payload_path|
+      add_payload_entry(payload_path, api_path)
+    end
+  end
+
+  private
+
+  def payload_entries
+    Dir[File.expand_path("payloads/#{operation_id}/*.y*ml", root_path)]
+  end
+
+  def add_payload_entry(payload_path, api_path)
+    payload = YAML.load_file(payload_path)
+    payload_name = File.basename(payload_path)
+
+    api_kind = extract_api_kind_from_payload_path(payload_path)
+
+    if api_kind == 'france_connect' && api_path.nil?
+      api_path = payload['api_path']
+    end
+
+    readme_stream.puts "* [#{payload_name}](#{payload_name})"
+    readme_stream.puts
+    readme_stream.puts "  Status `#{payload['status']}`"
+
+    if payload['description']
+      readme_stream.puts
+      readme_stream.puts "  #{payload['description']}"
+      readme_stream.puts
+    end
+
+    add_collapse_section(
+      'Paramètres',
+      "```json\n" \
+      "#{JSON.pretty_generate(payload['params'])}\n" \
+      "```"
+    )
+
+    add_collapse_section(
+      'Réponse API',
+      "```json\n" \
+      "#{JSON.pretty_generate(JSON.parse(payload['payload']))}\n" \
+      "```"
+    )
+
+    if api_path
+      add_collapse_section(
+        'Commande cURL',
+        "```bash\n" \
+        "#{generate_code_sample_from_path(api_path, payload['params'], api_kind)}\n" \
+        "```"
+      )
+    end
+  end
+
+  def add_title
+    readme_stream.puts "# #{title}"
+  end
+
+  def add_description
+    extra_description_path = File.expand_path("payloads/#{operation_id}/extra_description.md", root_path)
+
+    if File.exist?(extra_description_path)
+      readme_stream.puts
+      readme_stream.puts File.read(extra_description_path)
+      readme_stream.puts
+    end
+  end
+
+  def add_collapse_section(title, content)
+    readme_stream.puts
+    readme_stream.puts "  <details><summary>#{title}</summary>"
+    readme_stream.puts "  <p>"
+    readme_stream.puts
+    readme_stream.puts "  #{content.gsub("\n", "\n  ")}"
+    readme_stream.puts
+    readme_stream.puts "  </p>"
+    readme_stream.puts "  </details>"
+  end
+
+  def generate_code_sample_from_path(api_path, params, api_kind)
+    GenerateCodeSampleFromPath.new(api_path, params, api_kind).perform
+  end
+
+  def extract_api_kind_from_payload_path(payload_path)
+    case
+    when payload_path.include?('api_entreprise')
+      'api_entreprise'
+    when payload_path.include?('api_particulier')
+      'api_particulier'
+    when payload_path.include?('france_connect')
+      'france_connect'
+    else
+      raise 'Unknown API kind'
+    end
+  end
+
+  def readme_stream
+    @readme_stream ||= File.open(readme_path, 'w')
+  end
+
+  def readme_path
+    File.expand_path("payloads/#{operation_id}/README.md", root_path)
+  end
+end
